94b5ea83c8bf2bd1331107b17b64412287c5315f
Jeff Burgess <jeffreydavidburgess@gmail.com>
2014-08-12 10:46:41 -0700

    Update ajax-content-include.js

--- ajax-content-include.js
+++ ajax-content-include.js
@@ -1,13 +1,8 @@
-/* connection to Ajax jQuery library required */
-
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<!-- loads Ajax jQuery library -->
-
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<!-- loads jQuery UI library (must be loaded after jQuery library) -->
+/* connection to jQuery library required */
 
 
-// start AJAX includes
+
+
 
 $(document).ready(function() {
   // when one of the links with class "navlink" is clicked, get its "href" value
@@ -19,7 +14,20 @@ $(document).ready(function() {
     });
 });
 
-// end AJAX includes
+// begin HTML
 
+<ul>
+<li><a href="includes/home.html" class="navlink">Home</a></li>
+<li><a href="includes/one.html" class="navlink">One</a></li>
+<li><a href="includes/two.html" class="navlink">Two</a></li>
+<li><a href="includes/three.html" class="navlink">Three</a></li>
+</ul>
+
+
+<div class="ajax-content">
+
+<p>Content being swapped out goes here.</p>
+
+</div>
 
 
